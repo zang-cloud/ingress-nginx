4bdd26f5dc3e455f2809ca9f88f7ccf9139e2088
Heron Rossi <rossih@avaya.com>
2021-07-23 10:19:05 -0300

    General improvements

--- rootfs/lua/avaya_lrucache.lua
+++ rootfs/lua/avaya_lrucache.lua
@@ -7,7 +7,7 @@ local cache_ttl_seconds = 300 -- five minutes
 
 -- we need to initialize the cache on the lua module level so that
 -- it can be shared by all the requests served by each nginx worker process:
-local c = lrucache.new(20000)  -- allow up to 20k items in the cache
+local c = lrucache.new(10000)  -- allow up to 10k items in the cache
 if not c then
     return error("failed to create the cache: " .. (err or "unknown"))
 end
@@ -19,20 +19,22 @@ function _M.go()
     if is_authorized then
         return
     elseif is_authorized == false then
-        ngx.status = ngx.HTTP_UNAUTHORIZED
-        ngx.say("cached false")
-        ngx.exit(ngx.HTTP_UNAUTHORIZED)
+        ngx.status = ngx.HTTP_FORBIDDEN
+        ngx.say('') -- return an empty response instead of nginx default 403 HTMl response
+        ngx.exit(ngx.HTTP_FORBIDDEN)
         return
     end
 
-    res = ngx.location.capture('/validate-path?path=' .. ngx.var.request_uri, {share_all_vars = true, method = ngx.HTTP_GET})
-    ngx.log(ngx.ERR, "heron-test resStatus " .. res.status)
+    res = ngx.location.capture('/v1/blocked-paths/validate?path=' .. ngx.var.request_uri, {share_all_vars = true, method = ngx.HTTP_GET})
 
-    if res.status == ngx.HTTP_UNAUTHORIZED then
-      c:set(ngx.var.request_uri, false, cache_ttl_seconds)
-      ngx.status = ngx.HTTP_UNAUTHORIZED
-      ngx.say(ngx.var.request_uri)
-      ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    if res.status ~= ngx.HTTP_OK then
+      ngx.header.content_type = 'application/json';
+      if res.status == ngx.HTTP_FORBIDDEN then
+        c:set(ngx.var.request_uri, false, cache_ttl_seconds)
+      end
+      ngx.status = res.status
+      ngx.say(res.body)
+      ngx.exit(res.status)
     else
       c:set(ngx.var.request_uri, true, cache_ttl_seconds)
     end
